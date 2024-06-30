274de9df407795d649f5f58f324b18cecc9011c7
Ram742k <ramkumar742k@gmail.com>
2024-06-30 12:03:24 +0530

    add backend url1

--- src/services/instance.js
+++ src/services/instance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://password-reset-be-mgan.onrender.com',
+  baseURL: 'https://password-reset-be-9pu5.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },

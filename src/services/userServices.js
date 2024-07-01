1089d66b8430dbec98cf69cae0453f1fc5f5ea9c
Ram742k <ramkumar742k@gmail.com>
2024-07-01 09:26:32 +0530

    check1

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,24 +3,23 @@ import { instance } from './instance'; // Correct import statement
 
 // Define the user services
 const userServices = {
-  // Function to send email for password reset
-  getEmail: async (email) => {
-    
-    // Make a POST request to the reset-password endpoint
-    return await instance.post('/users/', { email });
-  },
-  postResetPassword: async (token, password) => {
-    return await instance.post(`/users/reset-password/${token}`, { password });
-  },
-  login: async (email, password) => {
+  
+  
+  
+login: async (email, password) => {
     // make a POST request to the login endpoint
     return await instance.post('/users/login', {
         email,
         password
     }, { withCredentials: true });
 },
+getEmail: async (email) => {
+  return await instance.post('/users/', { email });
+},
+postResetPassword: async (token, password) => {
+  return await instance.post(`/users/reset-password/${token}`, { password });
+},
 register: async (name, email, password) => {
-    
     // make a POST request to the register endpoint
     return await instance.post('/users/register', {
         name,
